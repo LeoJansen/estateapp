015761665c50f2497ec210977b6af01cba557dc9
Léo Jansen <leo@leoo.app>
2024-10-11 22:57:00 -0300

    updates

---
+++ api/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.routes.js";
+/* import postRoute from "./routes/post.routes.js";
+import testRoute from "./routes/test.routes.js";
+import userRoute from "./routes/user.routes.js";
+import chatRoute from "./routes/chat.routes.js";
+import messageRoute from "./routes/message.routes.js"; */
+
+const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+/* app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute); */
+
+app.listen(8800, () => {
+  console.log("Server is running!");
+});
